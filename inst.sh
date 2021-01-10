77dd3432900b77106f8dba60a81a0a1856eab308
modukurulakshmikarthikeya <71615750+modukurulakshmikarthikeya@users.noreply.github.com>
2021-01-10 09:10:28 +0530

    Add files via upload

---
+++ inst.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo chmod 777 google-chrome-stable_current_amd64.deb
+sudo apt-get install ./google-chrome-stable_current_amd64.deb
+sudo chmod 777 teamviewer_15.13.6_amd64.deb
+sudo apt-get install ./teamviewer_15.13.6_amd64.deb
